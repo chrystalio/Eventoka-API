a1b755b2169e41c2f44288b164c8ef376966e31f
Chrystalio <chrystalio.k@outlook.com>
2025-01-29 03:28:13 +0700

    Implement basic Express server with dummy endpoint

---
+++ src/controllers/dummy.controller.ts
@@ -0,0 +1,13 @@
+import { Request, Response } from "express";
+
+
+
+export default {
+    dummy(req: Request, res: Response) {
+        res.status(200).json({ 
+            message: "Success hit endpoint /dummy",
+            data: "OK"
+         });
+    }
+};
+
